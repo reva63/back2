22f87144ea54dfc8e97782bfb4773ba74e99e728
JcJet <jcjet88@gmail.com>
2024-10-03 22:55:15 +0300

    fix: params weren't parsed as numbers automatically

--- src/business/stages/dto/remove/removeStage.params.dto.ts
+++ src/business/stages/dto/remove/removeStage.params.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber } from 'class-validator';
+import { IsNumberString } from 'class-validator';
 import { IParamsDto } from 'src/core/abstract/base/dto/paramsDto.interface';
 
 export class RemoveStageParamsDto implements IParamsDto {
